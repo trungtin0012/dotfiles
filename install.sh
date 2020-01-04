5610d91ee36a986fdbfcc34425f7c7ed642c4d2b
Tin Nguyen <trungtinnguyen1994@gmail.com>
2020-01-04 18:08:11 +0700

    resource and update tmux config

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+echo "Installing dotfiles."
+
+source install/link.sh
+
+if test ! "$( command -v brew )"; then
+    echo "Installing homebrew"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+
+# install brew dependencies from Brewfile
+brew bundle
+
+# After the install, setup fzf
+echo -e "\\n\\nRunning fzf install script..."
+echo "=============================="
+/usr/local/opt/fzf/install --all --no-bash --no-fish
+
+# Change the default shell to zsh
+zsh_path="$( command -v zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
+
+if ! command_exists zsh; then
+    echo "zsh not found. Please install and then re-run installation scripts"
+    exit 1
+elif ! [[ $SHELL =~ .*zsh.* ]]; then
+    echo "Configuring zsh as default shell"
+    chsh -s "$(command -v zsh)"
+fi
+
+# Change the default shell to zsh
+zsh_path="$( command -v zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
+
+echo "Done. Reload your terminal."
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Dotfiles
 
 This is the collection of my vim, zsh, tmux configurations.
+The setup is only for macos
+
+install.sh is not tested yet.
+
+You should run install/backup.sh to backup your files first.
+
+Thanks to https://github.com/nicknisi/dotfiles for this configuration, please read the readme in nick repo for more details
